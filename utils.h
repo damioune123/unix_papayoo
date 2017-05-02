110004454b2d8aa60976a58ee998568608c3541f
damioune123 <damienmeur@gmail.com>
2017-05-02 15:10:42 +0200

    test client /serveur socket

--- utils.h
+++ utils.h
@@ -1,6 +1,9 @@
 #include <unistd.h>
 #include <stdio.h>
-#include <sys/types/h>
+#include <sys/types.h>
 #include <sys/socket.h>
+#include<unistd.h>
+#include<string.h>
+#include<arpa/inet.h>
 #define TRUE	1
 #define FALSE	0
