bf23204d48981bd65b7db416d32214dc8aa57eb7
damioune123 <damienmeur@gmail.com>
2017-05-03 04:24:32 +0200

    inscription alarme + select

--- server.c
+++ server.c
@@ -1,13 +1,20 @@
 #include "server.h"
+
 player players[MAX_PLAYERS];
 int port;
 FILE *fpError;
-int amount_players=0;
-int running = TRUE;
+int server_running, game_running, amount_players;
 int stock_addr_size = sizeof(struct sockaddr_in);
+message mess;
 int main(int argc , char *argv[])
 {
+        int i, max_fd, server_socket, select_res;
+        fd_set fds;
+        struct timeval timeout = {0, 15000};
+	struct sockaddr_in server_addr, client_addr;
         int fdLock = open("./server.lock", O_RDWR);
+        fct_ptr dispatcher[] = { };//USE TO DIRECTLY CALL FUNCTION WITH A CODE SENT FROM CLIENT
+
 	if (fdLock == -1) { 
 		perror("Erreur ouverture fichier lock\n");
 		exit(EXIT_FAILURE);
@@ -22,59 +29,94 @@ int main(int argc , char *argv[])
         }
         if(argc==3)
             fpError =freopen(argv[2], "a", stderr);
-        fprintf(stderr, "test\n");
-	int server_socket ;
-	struct sockaddr_in server_addr, client_addr;
+        port=atoi(argv[1]);
+        for( i=0; i < MAX_PLAYERS; i++){
+            players[i].socket=0;
+        }
+        amount_players=0;
+        server_running=TRUE;
+        game_running=FALSE;
+        struct sigaction alarm, interrupt;
+        memset(&alarm, 0, sizeof(alarm));
+	memset(&interrupt, 0, sizeof(interrupt));
+	alarm.sa_handler = &alarm_handler;
+	interrupt.sa_handler = &interrupt_handler;
+	sigaction(SIGALRM, &alarm, NULL);
+	sigaction(SIGINT, &interrupt, NULL);
         init_server(&server_socket, &server_addr);
-        while(running){
-            while(amount_players< MAX_PLAYERS){
-                add_player(&players[amount_players], server_socket);
+        while(server_running){
+            FD_ZERO(&fds);
+            FD_SET(server_socket, &fds);
+            max_fd = server_socket + 1;
+            for (i = 0; i < MAX_PLAYERS; i++) {
+                if (players[i].socket > 0) {
+                    FD_SET(players[i].socket, &fds);
+                }
+                if (players[i].socket >= max_fd) {
+                    max_fd = players[i].socket+1;
+                }
+            }
+            if ((select_res = select(max_fd, &fds, NULL, NULL, &timeout)) < 0) {
+                if (errno != EINTR) {
+                    shutdown_server();
+                    return EXIT_FAILURE;
+                }
+            }
+
+            if (select_res > 0) {
+                if (FD_ISSET(server_socket, &fds)) {
+                    add_client(server_socket, &client_addr);
+                }
+                for (i = 0; i < amount_players; i++) {
+                    if (FD_ISSET(players[i].socket, &fds)) {
+                        if (receive_msg(&mess, players[i].socket)) {
+                            dispatcher[mess.code] (players[i].socket, mess);
+                        } else {
+                            remove_player(players, i, FALSE);
+                        }
+                    }
+                }
+            }
+            if(game_running){
+                //TO DO
             }
         }
         if(argc==3)
             fclose(fpError);
-	return 0;
+        shutdown_socket(server_socket);
+	return EXIT_SUCCESS;
 }
-void add_player(player * newPlayer, int server_socket){
-	int read_size;
-        message recvClient;
-        message sendClient;
-	//Accept and incoming connection
-	printf("Waiting for incoming connections...\n");
 
-	//accept connection from an incoming client
-	newPlayer->socket =  accept(server_socket, (struct sockaddr *) &(newPlayer->client_addr), (socklen_t*)&stock_addr_size);
-	if (newPlayer->socket < 0)
-	{
-		perror("accept failed");
-		return ;
-	}
-	printf("Connection accepted\n");
+void add_client(int server_socket, struct sockaddr_in *cl_addr) {
+	int new_cl_socket;
+	int cl_addr_length = sizeof(struct sockaddr_in);
+	if ((new_cl_socket = accept(server_socket, (struct sockaddr *)cl_addr, (socklen_t*) &cl_addr_length)) < 0) {
+		perror("Connection error");
+		exit(EXIT_FAILURE);
+	} else {
+		if (game_running || amount_players == MAX_PLAYERS) {
+                        mess.code=C_REFUSE;
+                        strcpy(mess.payload,M_CONN_REFUSE);
+			send_message(mess, new_cl_socket);
 
-	//Receive a message from client with his name
-	while( (read_size = recv(newPlayer->socket , &recvClient, sizeof(message) , 0)) > 0 )
-	{
-                printf("debug recv: %s\n ",recvClient.payload);
-                sendClient.code=C_OK;
-                strcpy(sendClient.payload, M_SIGNUP_CLIENT_OK);
-		//Send the message back to client
-		write(newPlayer->socket ,&sendClient , sizeof(message));
-                break;
+		} else {
+			add_player(new_cl_socket);
+		}
 	}
-
-	if(read_size == 0)
-	{
-		fprintf(stderr, "Client disconnected\n");
-		fflush(stderr);
-	}
-	else if(read_size == -1)
-	{
-		perror("recv failed");
-	}
-        else
-            amount_players++;
-
 }
+
+void add_player(int socket) {
+        //TO DO / BIZNESS ATTRIBUTE SET (CARDS, ...)
+        players[amount_players++].socket = socket;
+        mess.code=C_REFUSE;
+        strcpy(mess.payload, M_SIGNUP_CLIENT_OK);
+	send_message(mess, socket);
+	if (amount_players == 1) {
+		//first client, set an alarm for 30 seconds
+		alarm(COUNTDOWN);
+	}
+}
+
 void init_server(int *server_socket, struct sockaddr_in *server_addr) {
 
 	if ((*server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
@@ -87,7 +129,7 @@ void init_server(int *server_socket, struct sockaddr_in *server_addr) {
 	//Prepare the sockaddr_in structure
         server_addr->sin_family = AF_INET;
 	server_addr->sin_addr.s_addr = INADDR_ANY;
-	server_addr->sin_port = htons(DAMIEN_PORT);
+	server_addr->sin_port = htons(port);
 
 	//Bind
 	if( bind(*server_socket,(struct sockaddr *)server_addr , sizeof(*server_addr)) < 0)
@@ -100,4 +142,78 @@ void init_server(int *server_socket, struct sockaddr_in *server_addr) {
 	listen(*server_socket , MAX_PLAYERS);
 }
 
+int receive_msg(message* msg, int fd) {
+	int bytes_received;
+	if ((bytes_received = recv(fd, msg, sizeof( message), 0)) <= 0) {
+		if (bytes_received == 0) {
+			fprintf(stderr, "Client disconnected.\n");
+		}
+		else {
+			perror("Could not receive message");
+		}
+		return FALSE;
+	}
+	return TRUE;
+}
 
+void shutdown_server() {
+	printf("server shutting down ..\n");
+	//clear_lobby(); ==> TO DO
+        server_running = FALSE;
+}
+
+void shutdown_socket(int socket) {
+	printf("Shutting down socket number %d\n", socket);
+	if (close(socket) < 0) {
+		perror("Socket shutdown");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void clear_lobby() {
+        //TO DO
+        /*
+	while (players[0].socket > 0) {
+	    remove_player(players, 0, FALSE);
+	}*/
+    printf("Enter clear lobby method \n");
+}
+
+
+void remove_player(player* players, int index, int sockopen) {
+    //TO DO
+    printf("Enter remove player method \n");
+}
+
+void send_message(message msg, int socket) {
+	if (send(socket, &msg, sizeof(struct message), 0) == -1) {
+		perror("Failed to send a mesesage to the socket");
+	}
+}
+void alarm_handler(int signum) {
+    if (signum == SIGALRM) {
+        if (amount_players < 2) {
+            clear_lobby();
+        } else {
+            start_game();
+        }
+    }
+}
+void interrupt_handler(int signum) {
+	if (signum == SIGINT) {
+		shutdown_server();
+	}
+}
+void start_game() {
+	start_round();
+	game_running = TRUE;
+}
+
+void start_round() {
+	deal_cards();
+        //TO DO
+}
+
+void deal_cards() {
+    //TO DO
+}
--- server.h
+++ server.h
@@ -1,11 +1,18 @@
 #ifndef SERVER_H
 #define SERVER_H
 #include "utils.h"
+#include <sys/select.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+
 
 #define EXIT_ERROR 1
 #define EXI_SUCCESS 0
+#define MIN_PLAYERS 2
 #define MAX_PLAYERS 4
-
+#define COUNTDOWN 30 //30 seconds te
+typedef void (*fct_ptr)( );
 typedef struct player {
 	int socket;
         struct sockaddr_in client_addr;
@@ -14,5 +21,16 @@ typedef struct player {
 
 #endif
 
-void add_player(player * , int);
 void init_server(int *, struct sockaddr_in *);
+void alarm_handler(int); //handles alarm timeouts
+void interrupt_handler(int); //shuts down the server when a SIGINT occurs
+void shutdown_socket(int); //closes a given socket
+void shutdown_server(); //halts the server
+void add_client(int, struct sockaddr_in*); //adds a client to the fdset
+void add_player(int); //confirm connection and inform client
+void remove_player(player*, int, int); //removes a player from the game
+int receive_msg(message*, int); //handles incoming messages
+void send_message(message, int);
+void start_game(); //starts the game
+void deal_cards(); //shuffles the deck and deals cards to all players
+void start_round(); //starts a new round
